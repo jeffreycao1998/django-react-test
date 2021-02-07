1748435bf407f66b1130f3aac2f693d8991c5fb9
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-02-06 23:40:19 -0500

    init a channels app and created a bare bones Channel model

---
+++ twitch_clone/channels/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Channel(models.model):
+  name = models.CharField("Name", max_length=240)
+  registrationDate = models.DateField("Registration Date", auto_now_add=True)
+
+  def __str__(self):
+    return self.name
