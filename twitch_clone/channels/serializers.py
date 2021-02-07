ea142f1d95a9a579ffae3b039f3e6d0818f44c53
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-02-07 10:22:17 -0500

    created CRUD endpoints to create a channel

---
+++ twitch_clone/channels/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Channel
+
+class ChannelSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Channel
+    fields = ('pk', 'name', 'registrationDate')
--- twitch_clone/twitch_clone/settings.py
+++ twitch_clone/twitch_clone/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'cordsheaders.middleware.CorsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
 ]
 
