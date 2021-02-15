7927628c5e51826e51ad522247fd49b361f26c6c
jeffreycao1998 <jeffreycao1998@hotmail.com>
2021-02-15 10:48:09 -0500

    added app to project settings and created db models

--- api/api/settings.py
+++ api/api/settings.py
@@ -33,6 +33,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
